ec336a996496798c76fedd8f325116c88ef5eb9e
Lebronfan42 <s10257669@connect.np.edu.sg>
2024-02-03 18:42:42 +0800

    updates to ranking page

--- JS-Files/PBar.js
+++ JS-Files/PBar.js
@@ -4,11 +4,35 @@ function updateProgressBar(progressBar, value) {
 
     progressFill.style.width = `${value}%`;
     progressBarText.textContent = `${value}%`;
+
+
+    localStorage.setItem("progressValue", value);
 }
 
-// Event listener for the '1' key
+
+document.addEventListener("DOMContentLoaded", function() {
+    const storedValue = localStorage.getItem("progressValue") || 0;
+    const myProgressBar = document.getElementById("myProgressBar");
+    updateProgressBar(myProgressBar, storedValue);
+});
+
+
 document.addEventListener("keydown", function(event) {
     if (event.key === "1") {
+        const myProgressBar = document.getElementById("myProgressBar");
+        updateProgressBar(myProgressBar, 20);
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "2") {
+        const myProgressBar = document.getElementById("myProgressBar");
+        updateProgressBar(myProgressBar, 40);
+    }
+});
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === "3") {
         const myProgressBar = document.getElementById("myProgressBar");
         updateProgressBar(myProgressBar, 60);
     }
