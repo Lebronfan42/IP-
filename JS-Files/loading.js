bd77851eff89e596badffefa12cbb274936bdc6b
Lebronfan42 <kishaan06raja@gmail.com>
2024-02-06 03:02:50 +0800

    added lottie loading screens

---
+++ JS-Files/loading.js
@@ -0,0 +1,7 @@
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load", function(){
+    setTimeout(function() {
+        loader.style.display = "none";
+    }, 1500);
+});
